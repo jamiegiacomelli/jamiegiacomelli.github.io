5197a5ab3842045e5634845437ac9044216ffac3
Pete Heaney <pete@peteheaney.com>
2015-08-12 11:27:44 +0100

    Updated popup height

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,7 +19,7 @@ $(function() {
 			shareUrl = "https://www.facebook.com/sharer/sharer.php?u="+curUrl;
 		}
 
-		windowPopup( shareUrl, 576, 300 );
+		windowPopup( shareUrl, 576, 500 );
 
 		return false;
 	});
