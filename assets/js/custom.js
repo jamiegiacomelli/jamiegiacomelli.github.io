6271de3f3718c4cbb983e56561ad97a3b5fbb6af
Pete Heaney <pete@peteheaney.com>
2015-08-12 11:17:10 +0100

    Added jQuery / facebook share functionality

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+// popup window
+function windowPopup(url, width, height) {
+	var left = (screen.width / 2) - (width / 2),
+			top = (screen.height / 2) - (height / 2);
+
+	window.open(url, "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left);
+}
+
+$(function() {
+	var curUrl = encodeURI( window.location.protocol + "//" + window.location.host + window.location.pathname );
+
+	$(".social-btn").on("click", function(e) {
+		var $this = $(this);
+		var shareUrl = '';
+
+		if( $this.data('site') == 'twitter' ) {
+			shareUrl = "https://twitter.com/intent/tweet/?url="+curUrl;
+		} else if( $this.data('site') == 'facebook' ) {
+			shareUrl = "https://www.facebook.com/sharer/sharer.php?u="+curUrl;
+		}
+
+		windowPopup( shareUrl, 500, 300 );
+
+		return false;
+	});
+});
